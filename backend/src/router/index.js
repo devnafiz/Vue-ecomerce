a23860223f4675ac09f7711af1f858ae1b9b22f2
nafiz <nafiz.2011@yahoo.com>
2023-03-21 12:50:24 +0600

    add  login

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from "vue-router";
 
 import Dashboard from "../view/Dashboard.vue";
 import Login from "../view/Login.vue";
+import forgotPassword from "../view/Forgot-Password.vue";
+
 
 
 const routes = [
@@ -14,6 +16,11 @@ const routes = [
     	path:'/login',
     	name:'login',
     	component: Login
+    },
+    {
+    	path:'/forgot-password',
+    	name:'forgot-password',
+    	component: forgotPassword
     },
 
 ];
